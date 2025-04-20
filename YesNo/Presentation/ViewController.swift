2e0bc72ec287d25f6bf55827d0ae3e2549597e59
Daniil Isakov <serverpgig@yandex.ru>
2025-04-20 22:30:39 +0300

    Layout UI in Storyboard, added shadows in code, connected all IBOs

--- YesNo/Presentation/ViewController.swift
+++ YesNo/Presentation/ViewController.swift
@@ -1,15 +1,35 @@
 import UIKit
+import WebKit
 
 final class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var actionButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        
+    }
+    
+    
+    
+    @IBAction func actionButtonClicked(_ sender: UIButton) {
     }
     
     private func setupUI() {
         setupGradientBackground()
+        setupButtonShadow()
+        
     }
+    
+    private func setupButtonShadow() {
+        actionButton.layer.shadowColor = UIColor.black.cgColor
+        actionButton.layer.shadowOpacity = 0.1
+        actionButton.layer.shadowOffset = CGSize(width: 0, height: 4)
+        actionButton.layer.shadowRadius = 8
+        actionButton.layer.masksToBounds = false
+    }
+    
     
     private func setupGradientBackground() {
         let gradientLayer = CAGradientLayer()
@@ -22,7 +42,7 @@ final class ViewController: UIViewController {
         
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
         gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
-
+        
         view.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
