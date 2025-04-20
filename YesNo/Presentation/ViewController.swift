a4f7c7f6f5f4f913a66eac077b605aa5ada25857
Daniil Isakov <serverpgig@yandex.ru>
2025-04-20 21:29:17 +0300

    Add image to launch screen and set background on main screen

--- YesNo/Presentation/ViewController.swift
+++ YesNo/Presentation/ViewController.swift
@@ -1,9 +1,25 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupGradientBackground()
+    }
+    
+    private func setupGradientBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        
+        gradientLayer.colors = [
+            UIColor(named: "BGColorOne")?.cgColor ?? UIColor.white.cgColor,
+            UIColor(named: "BGColorTwo")?.cgColor ?? UIColor.white.cgColor
+        ]
+        
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
 
