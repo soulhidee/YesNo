43ada9e4c5b38b33f1f3cd8fca49a535f72b0a63
Daniil Isakov <serverpgig@yandex.ru>
2025-04-20 19:14:25 +0300

    first commit

---
+++ YesNo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YesNo
+//
+//  Created by Даниил on 20.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
