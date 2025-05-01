373812e23aee3a1512e9b49a67007aedaeab573c
Daniil Isakov <serverpgig@yandex.ru>
2025-05-01 17:25:00 +0300

    refactor: Add dependencies and state property to presenter

--- YesNo/Presentation/MainPresenter.swift
+++ YesNo/Presentation/MainPresenter.swift
@@ -1,5 +1,8 @@
 import Foundation
 
 final class MainPresenter {
-    
+    private weak var view: MainViewProtocol?
+    private let gifLoader: GifLoading
+    private let soundManager: SoundManager
+    private var currentAnswer: String?
 }
