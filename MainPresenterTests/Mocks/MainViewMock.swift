a10cb3c624be84166366f323052fc1186668b2f2
Daniil Isakov <serverpgig@yandex.ru>
2025-05-01 22:06:05 +0300

    test: Add MainView mock for unit testing

---
+++ MainPresenterTests/Mocks/MainViewMock.swift
@@ -0,0 +1,26 @@
+import Foundation
+@testable import YesNo
+
+final class MainViewMock: MainViewProtocol {
+    func animateElementsOut() {
+        
+    }
+    
+    func loadGifInWebView(from urlString: String) {
+        
+    }
+    
+    func changeButtonTextColor() {
+        
+    }
+    
+    func setActivityIndicator(visible: Bool) {
+        
+    }
+    
+    func enableButton(_ enabled: Bool) {
+        
+    }
+    
+    
+}
