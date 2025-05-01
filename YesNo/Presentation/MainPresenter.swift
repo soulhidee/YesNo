9a5a97118fa9b91d8b47ed1a76b4e54ce94dd353
Daniil Isakov <serverpgig@yandex.ru>
2025-05-01 17:27:53 +0300

    refactor: Implement dependency injection via presenter initializer

--- YesNo/Presentation/MainPresenter.swift
+++ YesNo/Presentation/MainPresenter.swift
@@ -5,4 +5,14 @@ final class MainPresenter {
     private let gifLoader: GifLoading
     private let soundManager: SoundManager
     private var currentAnswer: String?
+    
+    init(
+        view: MainViewProtocol,
+        gifLoader: GifLoading = GifLoader(),
+        soundManager: SoundManager = SoundManager()
+    ) {
+        self.view = view
+        self.gifLoader = gifLoader
+        self.soundManager = soundManager
+    }
 }
