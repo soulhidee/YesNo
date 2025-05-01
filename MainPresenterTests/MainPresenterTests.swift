d4a3fc23fa402cc2221b2a58003784e6c583b98e
Daniil Isakov <serverpgig@yandex.ru>
2025-05-01 22:21:33 +0300

    test: Set up and tear down test environment

--- MainPresenterTests/MainPresenterTests.swift
+++ MainPresenterTests/MainPresenterTests.swift
@@ -3,4 +3,21 @@ import XCTest
 
 final class MainPresenterTests: XCTestCase {
     
+    var mainView: MainViewMock!
+    var gifLoader: GifLoaderMock!
+    var presenter: MainPresenter!
+    
+    override func setUp() {
+        super.setUp()
+        mainView = MainViewMock()
+        gifLoader = GifLoaderMock()
+        presenter = MainPresenter(view: mainView, gifLoader: gifLoader)
+    }
+    
+    override func tearDown() {
+        mainView = nil
+        gifLoader = nil
+        presenter = nil
+        super.tearDown()
+    }
 }
